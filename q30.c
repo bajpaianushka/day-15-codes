33ab67e4ba4f13876eb4056fd48a06c3d6311283
bajpaianushka <anushka.39934@stu.upes.ac.in>
2026-09-06 14:03:39 +0530

    Add files via upload

---
+++ q30.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	int n = 1234;
+	scanf("%d", &n);
+
+	int rev = 0;
+
+	while(n > 0){
+		int digit = n % 10;
+		rev = rev * 10 + digit;
+		n = n / 10;
+	}
+
+	printf("%d", rev);
+	return 0;
+}
